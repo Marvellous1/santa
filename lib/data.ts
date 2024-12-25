4b0ea0249e3ce09dbf97bc1185666113e83c62de
Marvellous1 <51808658+Marvellous1@users.noreply.github.com>
2024-12-25 20:30:38 +0100

    Add files via upload

--- lib/data.ts
+++ lib/data.ts
@@ -2,22 +2,78 @@ export type Person = {
   id: string;
   name: string;
   message: string;
-  reward: number;
+  reward: string;
   claimed?: boolean;
 };
 
 // Data store
-export const people: Person[] = [
+export const people: Person[] =  [
   {
-    id: "john-doe",
-    name: "John Doe",
-    message: "Thank you for being an amazing friend this year!",
-    reward: 200
+    id: "daddy",
+    name: "Daddy",
+    message: "Thank you for being an awesome father and for taking good care of us. For your provision and care, thank you sir.",
+    reward: "₦100,000",
   },
   {
-    id: "jane-smith",
-    name: "Jane Smith",
-    message: "Your support means the world to me. Merry Christmas!",
-    reward: 150
-  }
+    id: "mummy",
+    name: "Mummy",
+    message: "Thank you for your prayers, your love, your support, your care. We appreciate it all.",
+    reward: "₦100,000",
+  },
+  {
+    id: "aunt_j",
+    name: "Aunt J",
+    message: "Biggest Sister, thank you for being a great big sister and going out of your way to always be there for us all. We appreciate it big time.",
+    reward: "₦70,000",
+  },
+  {
+    id: "bro_peter",
+    name: "Bro Peter",
+    message: "Thank you for being a big support to our family. You’re a vital part of our family and we appreciate you.",
+    reward: "₦70,000",
+  },
+  {
+    id: "bro_ehis",
+    name: "Bro Ehis",
+    message: "Our fashionista brother. Thank you for being a source of inspiration and motivation that life can be awesome.",
+    reward: "$50",
+  },
+  {
+    id: "bro_obehi",
+    name: "Bro Obehi",
+    message: "Thank you for being a core anchor in the family. For your constant support and always coming through.",
+    reward: "$50",
+  },
+  {
+    id: "prisci",
+    name: "Prisci",
+    message: "Our small madam. You’re an awesome person. Thank you for all you do. For your constant love that you don’t hide. We appreciate you big time.",
+    reward: "₦50,000",
+  },
+  {
+    id: "marvellous",
+    name: "Marvellous",
+    message: "Thank you very much for being a wonderful person. Our Tech bro, blah blah blah. You’re an awesome guy.",
+    reward: "₦50,000",
+  },
+  {
+    id: "destiny",
+    name: "Destiny",
+    message: "Your growth has been awesome to watch. I’m proud of the person you’ve become. Keep building.",
+    reward: "₦30,000",
+  },
+  {
+    id: "bliss",
+    name: "Bliss",
+    message: "Small woman. Looking forward to seeing you grow.",
+    reward: "₦10,000",
+  },
+  {
+    id: "bryan",
+    name: "Bryan",
+    message: "Blu blah bah bah cu cu ca.",
+    reward: "₦5,000",
+  },
 ];
+
+
